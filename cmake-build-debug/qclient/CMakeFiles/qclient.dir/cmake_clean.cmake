df3f6d41d53ef253618556be0a729c2452a0327c
My Name <my-name@chromium.org>
2019-07-26 17:37:47 +0800

    Configured CLion on Windows

---
+++ cmake-build-debug/qclient/CMakeFiles/qclient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qclient.dir/client.c.o"
+  "qclient.pdb"
+  "qclient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/qclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/qserver/CMakeFiles/qserver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qserver.dir/qserver.c.o"
+  "qserver.pdb"
+  "qserver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/qserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
